59fc0cf330937cbed37dec653c44ac462ed6cea7
saranascimento <sarah.gomes80@hotmail.com>
2021-08-21 11:54:28 -0300

    Adiciona a biblioteca Miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,16 @@
+import React from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
+
+  
+
   return (
     <Container>
       <table>
